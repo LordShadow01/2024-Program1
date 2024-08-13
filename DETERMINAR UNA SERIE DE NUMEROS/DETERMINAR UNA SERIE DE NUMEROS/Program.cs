1f6906b42c47b7361cad5d61284a4c2e835d4e2a
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-08-13 16:40:32 -0600

    PROMEDIO DE UNA SERIE DE NUMEROS

---
+++ DETERMINAR UNA SERIE DE NUMEROS/DETERMINAR UNA SERIE DE NUMEROS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DETERMINAR_UNA_SERIE_DE_NUMEROS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma += num;
+            }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
+            //Pausa.
+            Console.ReadLine();
+        }
+    }
+}

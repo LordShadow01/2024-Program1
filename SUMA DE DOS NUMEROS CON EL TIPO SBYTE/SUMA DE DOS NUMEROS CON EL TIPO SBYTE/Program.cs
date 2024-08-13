69f8459e1dd523f3a46e188ded19287e1ef16129
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-08-13 16:06:06 -0600

    SUMA CON SBYTE

---
+++ SUMA DE DOS NUMEROS CON EL TIPO SBYTE/SUMA DE DOS NUMEROS CON EL TIPO SBYTE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA_DE_DOS_NUMEROS_CON_EL_TIPO_SBYTE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // suma de dos numeros SBYT
+
+            Console.Write("num 1:");
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+
+            Console.Write("num 2:");
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
+
+
+            int respuesta = num1 + num2;
+
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}

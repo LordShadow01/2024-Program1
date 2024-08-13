2f917e3c0affe82a88b142a33c01499b6a6bd6bc
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-08-13 16:53:19 -0600

    SI ES MAYOR O MENOR DE EDAD

---
+++ ESTRUCTURA DE CONTROL CON EDAD/ESTRUCTURA DE CONTROL CON EDAD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTRUCTURA_DE_CONTROL_CON_EDAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. if. ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
+            Console.Write("Edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
+            }
+            else
+            {
+                Console.WriteLine("Eres un adolescente, disfruta de la vida.");
+                //Pausa.
+                Console.ReadLine();
+            }
+        }
+    }
+}

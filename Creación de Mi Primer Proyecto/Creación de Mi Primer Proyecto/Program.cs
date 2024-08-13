cf1de5270349bb8bd50e78d71bcb82ce399d5f03
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-08-13 15:52:28 -0600

    Hola Mundo

---
+++ Creación de Mi Primer Proyecto/Creación de Mi Primer Proyecto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creación_de_Mi_Primer_Proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
+        }
+    }
+}

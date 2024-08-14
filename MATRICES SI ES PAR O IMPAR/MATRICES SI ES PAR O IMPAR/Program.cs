531672588a4cbb074dec3c44dfcf934531bca23a
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-08-14 10:13:14 -0600

    serie con numeros

---
+++ MATRICES SI ES PAR O IMPAR/MATRICES SI ES PAR O IMPAR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATRICES_SI_ES_PAR_O_IMPAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determinar si cada numero de la serie es par o impar
+
+            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+            foreach (int num in serie)
+            {
+                Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "par" : "impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
